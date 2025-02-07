de529b9b1af8846b164ef652b741dcfeeaf58776
iamzebula <zebula@uw.edu>
2025-02-06 20:55:50 -0800

    class with comments

--- Code/python/rate_kinetics_final_class.py
+++ Code/python/rate_kinetics_final_class.py
@@ -4,64 +4,146 @@ from scipy.optimize import curve_fit
 import os
 import pandas as pd
 
-class KineticDataAnalysis:
-    def __init__(self, data_path):
-        self.data_path = data_path
-        os.chdir(self.data_path)
-    
-    @staticmethod
-    def import_clean_data(file_name, sheet=None, columns=None, is_csv=False):
-        if is_csv:
-            data = pd.read_csv(file_name, header=None)
-        else:
-            data = pd.read_excel(file_name, sheet_name=sheet)
-        return data.iloc[:, columns].dropna()
-    
-    @staticmethod
-    def baseline_steadystate_response(t, y_initial, y_final, kon):
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import os
+
+class KineticAnalysis:
+    def __init__(self, file_name=None, sheet=None, columns=None):
+        self.file_name = file_name
+        self.sheet = sheet
+        self.columns = columns
+        self.data = None
+
+    # Import Data + Clean Data
+    def import_clean_data(self):
+        data = pd.read_excel(self.file_name, sheet_name=self.sheet)
+        self.data = data.iloc[:, self.columns].dropna()
+        return self.data
+
+    def import_clean_data_csv(self):
+        data = pd.read_csv(self.file_name, header=None)
+        self.data = data.iloc[:, self.columns].dropna()
+        return self.data
+
+    # Function 1: Baseline falling to steady state response
+    def baseline_steadystate_response(self, t, y_initial, y_final, kon):
+        """
+        Function to find the kon value from the data. 
+        Assuming we know the baseline and the steady state response, we can find the kon value.
+        The equation is y(t) = y_final * (1 - exp(-kon * t)) + y_intial
+
+        Parameters
+        ----------
+        data : pandas dataframe
+            Dataframe containing the time and response values
+        y_intial : float
+            The baseline value of the response
+        y_final : float
+            The steady state value of the response
+        t : float
+            The time value
+
+        Returns
+        -------
+        function
+            The function that can be used to calculate the response
+        """
         return y_final * (1 - np.exp(-kon * t)) + y_initial
 
-    @staticmethod
-    def response_to_zero(t, C, y_initial, kon, koff):
+    # Function 2: Response falling to zero
+    def response_to_zero(self, t, C, y_initial, kon, koff):
+        """
+        Function to find the kon and koff values from the data
+        Assuming we know that the response goes to zero
+        
+        Parameters
+        ----------
+        data : pandas dataframe
+            Dataframe containing the time and response values
+        C : float
+            The initial rate of signaling
+        y_initial : float
+            The baseline value of the response
+        t : float
+            The time value
+        
+        Returns
+        -------
+        function
+            The function that can be used to calculate the response
+        """
         return (C / (kon - koff)) * (np.exp(-koff * t) - np.exp(-kon * t)) + y_initial
 
-    @staticmethod
-    def response_to_steady_state(t, y_initial, y_final, D, kon, koff):
-        return y_final * ((1 - D * np.exp(-kon * t)) + (D - 1) * np.exp(-koff * t)) + y_initial
+    # Function 3: Response falling to steady state response
+    def response_to_steady_state(self, t, y_initial, y_final, D, kon, koff):
+        """
+        Function to find the kon and koff values from the data
+        Assuming we know the steady state response
+        
+        Parameters
+        ----------
+        data : pandas dataframe
+            Dataframe containing the time and response values
+        y_initial : float
+            The initial rate of signaling
+        y_final : float
+            The final rate of signaling
+        t : float
+            The time value
+        
+        Returns
+        -------
+        function
+            The function that can be used to calculate the response
+        """
+        return y_final * (1 - D * np.exp(-kon * t) + (D - 1) * np.exp(-koff * t)) + y_initial
 
-    @staticmethod
-    def typical_association(t, y_final, conc, kon, koff):
+    # Function 4: Typical association
+    def typical_association(self, t, y_final, conc, kon, koff):
+        """
+        Function to find the kon and koff values from the data
+        Assuming it is a typical association function
+        
+        Parameters
+        ----------
+        data : pandas dataframe
+            Dataframe containing the time and response values
+        y_final : float
+            The final rate of signaling
+        t : float
+            The time value
+        conc : float
+            The concentration of the substance
+        
+        Returns
+        -------
+        function
+            The function that can be used to calculate the response
+        """
         kd = koff / kon
         return ((y_final * conc) / (kd + conc)) * (1 - np.exp((-1 * (kon * conc + koff)) * t))
 
-    @staticmethod
-    def typical_dissociation(t, y_initial, koff):
+    # Function 5: Typical dissociation
+    def typical_dissociation(self, t, y_initial, koff):
+        """
+        Function to find the koff values from the data
+        Assuming it is a typical dissociation function
+        
+        Parameters
+        ----------
+        data : pandas dataframe
+            Dataframe containing the time and response values
+        y_initial : float
+            The initial rate of signaling
+        t : float
+            The time value
+        
+        Returns
+        -------
+        function
+            The function that can be used to calculate the response
+        """
         return y_initial * np.exp(-koff * t)
-    
-    def plot_fitted_curve(self, function, data, params):
-        plt.plot(data.iloc[:, 0], data.iloc[:, 1], 'o', label='Experimental Data')
-        fitted_response = function(data.iloc[:, 0], *params)
-        plt.plot(data.iloc[:, 0], fitted_response, '-', label='Fitted Curve')
-        plt.xlabel('Time (t)')
-        plt.ylabel('Response')
-        plt.title('Data and Fitted Curve')
-        plt.legend()
-        plt.show()
-        print("Fitted parameters:", params)
-    
-    def fit_data(self, time, response, p0, assumption, data):
-        function_map = {
-            "baseline+steadystate": self.baseline_steadystate_response,
-            "response to zero": self.response_to_zero,
-            "response to steady state": self.response_to_steady_state,
-            "typical_association": self.typical_association,
-            "typical_dissociation": self.typical_dissociation
-        }
-        
-        function = function_map.get(assumption)
-        if function is None:
-            raise ValueError("Invalid assumption type")
-        
-        params, pcov = curve_fit(function, time, response, p0=p0)
-        self.plot_fitted_curve(function, data, params)
-        return params, pcov
